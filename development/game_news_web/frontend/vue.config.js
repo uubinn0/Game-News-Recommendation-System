152a1caf7cb9e43c330da8465fca7b052c3449fc
DH Kim <tbvjekgus@gmail.com>
2024-11-20 05:20:08 +0000

    .

--- development/game_news_web/frontend/vue.config.js
+++ development/game_news_web/frontend/vue.config.js
@@ -1,7 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
+    transpileDependencies: true,
+
     devServer: {
         allowedHosts: "all"
+    },
+
+    pluginOptions: {
+      vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
     }
 })
